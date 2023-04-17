abbee6558193daba27ec3ca1efa1927155d77ea4
Jozinn <sikora10g@gmail.com>
2023-04-18 00:20:59 +0200

    App styled

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+const commentButtons = document.querySelectorAll('.comment-btn');
+
+for (commentButton in commentButtons) {
+    commentButton.addEventListener('click', e => {
+        e.target.parentNode.nextSibling.classList.toggle('hidden');
+    });
+}
